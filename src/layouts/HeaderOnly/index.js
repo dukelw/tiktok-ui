34757ef7b0a08f3745f1e81599802aaec7d478dc
dukelw <thevi16102004@gmail.com>
2023-12-10 13:20:39 +0700

    second commit

--- src/layouts/HeaderOnly/index.js
+++ src/layouts/HeaderOnly/index.js
@@ -1,17 +1,1 @@
-import PropTypes from 'prop-types';
-import Header from '../components/Header';
-
-function HeaderOnly({ children }) {
-  return (
-    <div>
-      <Header />
-      <div className="container">{children}</div>
-    </div>
-  );
-}
-
-HeaderOnly.propTyles = {
-  children: PropTypes.node.isRequired
-}
-
-export default HeaderOnly;
+export { default } from './HeaderOnly';
